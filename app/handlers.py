b46b7bc87716e992bed987db163c102c5e4cd39e
Anton K <kakkazak@gmail.com>
2021-11-01 21:25:20 +0200

    Add get and create stream handlers

--- app/handlers.py
+++ app/handlers.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
+from app.models import Stream, connect_db
+from app.forms import StreamForm
 
 router = APIRouter()
 
@@ -11,3 +13,17 @@ async def get_user():
 @router.post('/user', name='user:create')
 async def create_user(body=Body(..., embed=True)):
     return {'user': None}
+
+
+@router.get('/stream')
+def get_stream(session=Depends(connect_db)):
+    all_streams = session.query(Stream).all()
+    return all_streams
+
+
+@router.post('/stream')
+def create_stream(item: StreamForm = Body(..., embed=True), session=Depends(connect_db)):
+    stream = Stream(title=item.title, topic=item.topic)
+    session.add(stream)
+    session.commit()
+    return {'stream': {'status': 'created'}}
