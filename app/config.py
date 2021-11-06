3014030d800cfb5448de937a04bc48a58c7f2d2f
Anton K <kakkazak@gmail.com>
2021-11-06 15:58:45 +0200

    Add docker, change db url

--- app/config.py
+++ app/config.py
@@ -8,11 +8,12 @@ API_PREFIX = '/api'
 VERSION = '0.0.1'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+root_dir = dir_path[:-3]
 
-config = Config(f'{dir_path[:-3]}.env')
+config = Config(f'{root_dir}.env')
 
 DEBUG = config('DEBUG', cast=bool, default=False)
-DATABASE_URL = config('DB_CONNECTION', cast=str)
+DATABASE_URL = f'sqlite:///{root_dir}' + config('DB_NAME', cast=str)
 MAX_CONNECTIONS_COUNT = config('MAX_CONNECTIONS_COUNT', cast=int, default=10)
 MIN_CONNECTIONS_COUNT = config('MIN_CONNECTIONS_COUNT', cast=int, default=10)
 
